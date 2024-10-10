8ec2c5580b19b1ffd40b4818a027adc445fed596
Nadav Rahimi <nadav@cloudflare.com>
2024-10-10 13:31:34 +0100

    parse question

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
-use std::{
-    error::Error,
-    io::{Cursor, Read},
-    net::UdpSocket,
-};
+use std::{error::Error, net::UdpSocket};
 
-// TODO CLI command to bind to a specific (host, port) combination
-// TODO Support security awareness
-
-// DNS based on RFC 1035 and RFC 2535 but with
-// no support for security awareness
+// DNS based largely on RFC 1035 which
+// supports only questions and answers
 mod dns {
-    use std::error::Error;
+    use std::{
+        error::Error,
+        io::{Cursor, Read},
+    };
 
     mod flags {
         // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        // |QR|   Opcode  |AA|TC|RD|RA| Z|AD|CD|   RCODE   |
+        // |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
         // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
         pub const QR: u8 = 0b10000000;
         pub const OPCODE: u8 = 0b01111000;
         pub const AA: u8 = 0b00000100;
         pub const TC: u8 = 0b00000010;
         pub const RD: u8 = 0b00000001;
-        pub const Z: u8 = 0b01000000;
-        pub const AD: u8 = 0b00100000;
-        pub const CD: u8 = 0b00010000;
+        pub const RA: u8 = 0b10000000;
+        // (the Z bits are reserved) 0b01110000;
         pub const RCODE: u8 = 0b00001111;
     }
 
@@ -75,30 +70,22 @@ mod dns {
 
     #[derive(Debug)]
     pub struct Header {
-        id: u16,
-        is_query: bool, // If false, this is a response
-        opcode: Opcode,
-        is_authoritative_answer: bool,
-        truncated: bool,
-        recursion_desired: bool,
-        recursion_available: bool,
-        response_code: ResponseCode,
-        questions: u16,
-        answers: u16,
-        nameservers: u16,
-        additional: u16,
+        pub id: u16,
+        pub is_query: bool, // If false, this is a response
+        pub opcode: Opcode,
+        pub is_authoritative_answer: bool,
+        pub truncated: bool,
+        pub recursion_desired: bool,
+        pub recursion_available: bool,
+        pub response_code: ResponseCode,
+        pub questions: u16,
+        pub answers: u16,
     }
 
     impl Header {
         pub fn parse(buf: &[u8]) -> Result<Header, Box<dyn Error>> {
             if buf.len() != 12 {
                 return Err(Box::<dyn Error>::from("Slice is not 12 bytes long"));
-            }
-            // As per the DNS RFC, the Z value must
-            // be zero for all queries and responses
-            let z = buf[3] & flags::Z;
-            if z > 0 {
-                return Err(Box::<dyn Error>::from("Z is not zeroed"));
             }
 
             Ok(Self {
@@ -108,7 +95,7 @@ mod dns {
                 id: u16::from_be_bytes([buf[0], buf[1]]),
 
                 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-                // |QR|   Opcode  |AA|TC|RD|RA| Z|AD|CD|   RCODE   |
+                // |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
                 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
                 is_query: (buf[2] & flags::QR) > 0,
                 opcode: Opcode::try_from(u8::from_be((buf[2] & flags::OPCODE) << 1))?,
@@ -123,19 +110,103 @@ mod dns {
                 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
                 // |                    ANCOUNT                    |
                 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-                // |                    NSCOUNT                    |
-                // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-                // |                    ARCOUNT                    |
-                // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
                 questions: u16::from_be_bytes([buf[4], buf[5]]),
                 answers: u16::from_be_bytes([buf[6], buf[7]]),
-                nameservers: u16::from_be_bytes([buf[8], buf[9]]),
-                additional: u16::from_be_bytes([buf[10], buf[11]]),
             })
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum QType {
+        A,
+    }
+
+    impl TryFrom<u16> for QType {
+        type Error = String;
+
+        fn try_from(kind: u16) -> Result<Self, Self::Error> {
+            match kind {
+                1 => Ok(Self::A),
+                _ => Err(format!("invalid qtype: {kind}")),
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum QClass {
+        Internet,
+    }
+
+    impl TryFrom<u16> for QClass {
+        type Error = String;
+
+        fn try_from(class: u16) -> Result<Self, Self::Error> {
+            match class {
+                1 => Ok(Self::Internet),
+                _ => Err(format!("invalid qclass: {class}")),
+            }
+        }
+    }
+
+    // FIX I would like a Question::parse() method, but
+    //     the question doesn't have a set length, so I
+    //     can't provide it a pre-read slice using Cursor
+    #[derive(Debug)]
+    pub struct Question {
+        name: Vec<String>,
+        qtype: QType,
+        qclass: QClass,
+    }
+
+    #[derive(Debug)]
+    pub struct Message {
+        header: Header,
+        questions: Vec<Question>,
+    }
+
+    impl Message {
+        pub fn parse(buf: &[u8]) -> Result<Message, Box<dyn Error>> {
+            let mut cursor = Cursor::new(&buf[..]);
+
+            // Parse the header
+            let mut header_buf = [0; 12];
+            cursor.read_exact(&mut header_buf)?;
+            let header = Header::parse(&header_buf)?;
+
+            // Parse the questions
+            let mut questions: Vec<Question> = Vec::new();
+            for _ in 0..header.questions {
+                let mut labels: Vec<String> = Vec::new();
+                let mut name_len_buf = [0; 1];
+                cursor.read_exact(&mut name_len_buf)?;
+                while name_len_buf[0] != 0 {
+                    let mut label_buf = vec![0; name_len_buf[0] as usize];
+                    cursor.read_exact(&mut label_buf)?;
+                    labels.push(String::from_utf8(label_buf)?);
+                    cursor.read_exact(&mut name_len_buf)?;
+                }
+
+                let mut qtype_buf = [0; 2];
+                let mut qclass_buf = [0; 2];
+                cursor.read_exact(&mut qtype_buf)?;
+                cursor.read_exact(&mut qclass_buf)?;
+
+                let q = Question {
+                    name: labels,
+                    qtype: QType::try_from(u16::from_be_bytes(qtype_buf))?,
+                    qclass: QClass::try_from(u16::from_be_bytes(qclass_buf))?,
+                };
+                questions.push(q);
+            }
+
+            //
+
+            Ok(Self { header, questions })
         }
     }
 }
 
+// Run using `cargo run & dig @127.0.0.1 -p 5300 fiwippi.net`
 fn main() -> Result<(), Box<dyn Error>> {
     // Bind to a UDP socket
     let socket = UdpSocket::bind("127.0.0.1:5300")?;
@@ -145,14 +216,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // as part of RFC 1035
     let mut buf = [0; 512];
     let (size, _) = socket.recv_from(&mut buf)?;
-    let mut cursor = Cursor::new(&buf[..size]);
 
-    // Parse the header
-    let mut header_buf = [0; 12];
-    cursor.read_exact(&mut header_buf)?;
-    let header = dns::Header::parse(&header_buf)?;
-
-    println!("{header:?}");
+    // Parse the message
+    let msg = dns::Message::parse(&buf[..size]);
+    println!("{msg:?}");
 
     Ok(())
 }
